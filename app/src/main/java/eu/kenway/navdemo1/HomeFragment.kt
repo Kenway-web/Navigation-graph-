c573de0866e8aa3e33c7bc74cc7aed8fc1f1d0af
Shoaib Ali <shoaibali26021999@gmail.com>
2022-08-08 19:22:18 +0530

    Navigation graph example

---
+++ app/src/main/java/eu/kenway/navdemo1/HomeFragment.kt
@@ -0,0 +1,38 @@
+package eu.kenway.navdemo1
+
+import android.os.Bundle
+import android.text.TextUtils
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Toast
+import androidx.core.os.bundleOf
+import androidx.databinding.DataBindingUtil
+import androidx.navigation.findNavController
+import eu.kenway.navdemo1.databinding.FragmentHomeBinding
+
+class HomeFragment : Fragment() {
+private lateinit var binding: FragmentHomeBinding
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        binding=DataBindingUtil.inflate(inflater,R.layout.fragment_home, container, false)
+        binding.button.setOnClickListener {
+            if(!TextUtils.isEmpty(binding.editText.text.toString())) {
+                val bundle: Bundle = bundleOf("user_input" to binding.editText.text.toString())
+                it.findNavController().navigate(R.id.action_homeFragment_to_secondFragment2, bundle)
+            }
+            else
+            {
+                Toast.makeText(activity,"Please insert your name",Toast.LENGTH_LONG).show()
+            }
+        }
+        return  binding.root
+    }
+
+
+}
